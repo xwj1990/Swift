07f1947ead1cc71e385ef19b662dfbb5d9364e3a
baijiahulian <xiangwenjun@baijiahulian.com>
2015-08-19 23:08:54 +0800

    first commit

---
+++ swift-main/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-main
+//
+//  Created by baijiahulian on 15/8/19.
+//  Copyright (c) 2015年 BJHL. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
